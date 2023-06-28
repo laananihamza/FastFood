686990e5335a15450693314f68288d8f2b4a8509
laananihamza <loenwi03@gmail.com>
2023-06-28 17:55:10 +0100

    fix total quantity of products

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -19,14 +19,23 @@ class cartController extends Controller
                 session(['cart_id' => $cart->id]);
             }
             $cart = Cart::find(session()->get('cart_id'));
+            $cart_items = CartItems::where('id', $cart->id)->where('product_id', $request->product_id)->get();
+            return response()->json($cart_items);
+
             DB::table('cart_items')->insert(['cart_id' => $cart->id, 'product_id' => $request->product_id, 'quantity' => $request->quantity, 'created_at' => new DateTime(), 'updated_at' => new DateTime()]);
         } else {
             $cart = DB::table('carts')->find($request->user()['id']);
+            $cart_items = DB::table('cart_items')->where('cart_id', '=', $cart->id)->where('product_id', '=', $request->product_id)->get();
+            // return response()->json($cart_items);
+            if (count($cart_items) !== 0) {
+                DB::table('cart_items')->where('cart_id', '=', $cart->id)->where('product_id', '=', $request->product_id)->update(['quantity' => $cart_items[0]->quantity + 1]);
+            } else {
+                if (!$cart) {
 
-            if (count($cart) === 0) {
-                DB::table('carts')->insert(['user_id' => $request->user()['id']]);
+                    $cart = Cart::create(['user_id' => $request->user()['id']]);
+                }
+                DB::table('cart_items')->insert(['cart_id' => $cart->id, 'product_id' => $request->product_id, 'quantity' => $request->quantity, 'created_at' => new DateTime(), 'updated_at' => new DateTime()]);
             }
-            DB::table('cart_items')->insert(['cart_id' => $cart->id, 'product_id' => $request->product_id, 'quantity' => $request->quantity, 'created_at' => new DateTime(), 'updated_at' => new DateTime()]);
         }
     }
 
@@ -36,6 +45,7 @@ class cartController extends Controller
             $cart = DB::table('carts')
                 ->find(session()->get('cart_id'));
             $product_list = DB::table('products')->join('cart_items', 'cart_items.product_id', '=', 'products.id')->where('cart_id', '=', $cart->id ?? null)->get(); // join with cart_items
+
             // return $cart;
         } else {
             $cart = DB::table('carts')
--- resources/js/components/cartList.jsx
+++ resources/js/components/cartList.jsx
@@ -4,21 +4,25 @@ import { useEffect, useRef, useState } from "react";
 function CartList({click}) {
     const [cartClicked, setCartClicked] = useState(false)
 
-    /* Quantity State */
-    const [quantity, setQuantity] = useState(1)
-
+    
     /* Products State */
     const [products, setProducts] = useState([])
+    /* Quantity State */
+    const [quantity, setQuantity] = useState(0)
 
-    
+    /* Total Price */ 
+    const [Totalprice, setTotalPrice] = useState(0)
+
+    let total = 0
+    let quantityTotal = 0
     const increment = () => {
-        setQuantity((prev) => prev + 1)
+        // setQuantity((prev) => prev + 1)
     }
     const decrement = () => {
-        setQuantity((prev) => {
-            if (prev <= 1) return 1
-            return prev - 1
-        })
+        // setQuantity((prev) => {
+        //     if (prev <= 1) return 1
+        //     return prev - 1
+        // })
     }
     function getCartItems() {
         axios.get(route('cartItems')).then((res) => setProducts(res.data[0]))
@@ -37,25 +41,35 @@ function CartList({click}) {
             
         })
         getCartItems()
+        
+        
         return;
     }, [cartRef, click])
+    useEffect(() => {
+        for (let i = 0; i < products.length; i++) {
+            total += +products[i]?.price * +products[i]?.quantity;
+            quantityTotal += +products[i]?.quantity;
+        }
+        setTotalPrice(total)
+        setQuantity(quantityTotal)
+    }, [products])
     return ( 
         <>
             <span className="relative group" id="cart" ref={cartRef} onClick={() => setCartClicked((prev) => !prev)}>
                             <i className="las la-shopping-cart cursor-pointer border border-slate-400 rounded-full p-2 hover:bg-yellow-400  hover:border-yellow-400"></i>
-                            <span className="bg-yellow-500  absolute py-0.5 top-0 right-0 rounded-full font-bold text-xs h-5 w-5 text-center text-white" name="cart">{products?.length}</span>
-                            <div className={`producstOnCart ${cartClicked ? 'block' : 'hidden'}  group-hover:block bg-white absolute top-12 -right-20 border-t-[2.5px] border-black py-4 px-3 w-[350px]`}>
-                                <div className="products h-[250px] overflow-y-auto">
+                            <span className="bg-yellow-500  absolute py-0.5 top-0 right-0 rounded-full font-bold text-xs h-5 w-5 text-center text-white" name="cart">{quantity}</span>
+                            <div className={`producstOnCart ${cartClicked ? 'block' : 'hidden'}  group-hover:block bg-white absolute top-12 -right-20 border-t-[2.5px] border-black py-4 px-3 w-[400px]`}>
+                                <div className="products h-[300px] overflow-y-auto">
                                     <ul>
                                         {products.length !== 0 ? products?.map((product, i) => (
-                                            <li className="flex justify-between items-center border-t py-2 border-black" key={i}>
+                                            <li className="flex justify-between items-center first:border-t-0 border-t py-2 px-5 border-slate-200" key={i}>
                                             <img src={`/${product?.urlPhoto}`} className="w-20 rounded-full" alt="" />
                                             <div className="proInfo">
                                                 <p className="mb-1 text-base">{product?.name}</p>
                                                 <p className="text-lg mb-3 text-green-500">MAD <span className="priceCart"> {product?.price ? product?.price  : 0}</span></p>
                                                 <div className="counts flex">
                                                     <button className="dec py-1 border text-sm px-5 cursor-pointer" onClick={decrement}>-</button>
-                                                    <p className="dec py-1 border-y text-sm px-5"><span className="countMany">{quantity}</span></p>
+                                                    <p className="dec py-1 border-y text-sm px-5"><span className="countMany">{product?.quantity}</span></p>
                                                     <button className="dec py-1 border text-sm px-5 cursor-pointer" onClick={increment}>+</button>
                                                 </div>
                                             </div>
@@ -66,7 +80,7 @@ function CartList({click}) {
                                 </div>
         
                                 <div className="checkout border-t border-slate-400 text-center p-2 mt-3">
-                                    <p className="total-price text-slate-500 text-base mb-2">Total: <span id="Tprice">43 MAD</span></p>
+                                    <p className="total-price text-slate-500 text-base mb-2">Total: <span id="Tprice">{Totalprice} MAD</span></p>
                                     <button className="bg-slate-900 w-full py-3 text-white text-base mb-2"><Link href="/checkout">Checkout</Link></button>
                                     <button className="bg-white  border-2 hover:border-[3px] hover:py-[11px] border-slate-900 w-full py-3 text-slate-900 text-base">Checkout</button>
                                 </div>
